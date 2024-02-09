5330219c850da3549538d2eeb4538bbf83c48a78
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2024-02-09 16:40:57 +0100

    add button component

--- src/app/protected/dashboard/page.tsx
+++ src/app/protected/dashboard/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
+import { Button } from "@/app/components/Button";
 
 const DashboardPage = () => {
   return (
-    <div>This is the dashboard!</div>
-  )
-}
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is a paragraph in the dashboard</p>
+      <Button onClick={() => alert("hello")} text="Click me" />
+    </div>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
