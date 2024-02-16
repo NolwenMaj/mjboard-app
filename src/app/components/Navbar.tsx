c386d30992836bbd55c9f20492984f2d76498837
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2024-02-16 11:46:56 +0100

    remove dropdown, fix navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,27 +1,56 @@
-import React from 'react';
-import { authOptions } from '../api/auth/[...nextauth]/route';
-import { getServerSession } from 'next-auth/next';
-import Link from 'next/link';
-import Dropdown from './Dropdown';
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getServerSession } from "next-auth/next";
+import Link from "next/link";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 const Navbar = async () => {
-    const session = await getServerSession(authOptions);
-    let tabToDisplay = session && session.user?.email 
-    ? [{name: 'SignOut', path: '/auth/signout'},{name: 'Dashboard', path: '/protected/dashboard'}]
-    : [{name: 'SignIn', path: '/auth/signin'},{name: 'SignUp', path: '/auth/signup'}] 
+  const session = await getServerSession(authOptions);
 
-    return (
-        <div className='w-full px-4 py-8 bg-gray-300 flex flex-row items-center gap-4'>
-           
-            <Link href='/'>🏠</Link>
-            <Dropdown tabs={tabToDisplay}/>
-            {session && session.user?.email ? (
-                    <p>
-                        <b>Connecté.e avec {session.user?.email}</b>
-                    </p>):(<></>)}
+  return (
+    <header className="flex items-center h-14 px-4 border-b shrink-0 md:px-6 gap-5">
+      <Link href="/">🏠</Link>
+      {session && session.user?.email ? (
+        <div className="flex flex-row gap-5 justify-between w-full">
+          <div className="flex gap-5 items-center">
+            <Link
+              className="text-gray-500 dark:text-gray-400"
+              href="/auth/signout"
+            >
+              Se déconnecter
+            </Link>
+            <Link
+              className="text-gray-500 dark:text-gray-400"
+              href="/protected/dashboard"
+            >
+              Dashboard
+            </Link>
+          </div>
+          <div className="flex flex-row items-center gap-1 justify-end">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p>{session.user?.email}</p>
+          </div>
         </div>
-    );
+      ) : (
+        <div className="flex gap-5">
+          <Link
+            className="text-gray-500 dark:text-gray-400"
+            href="/auth/signin"
+          >
+            Se connecter
+          </Link>
+          <Link
+            className="text-gray-500 dark:text-gray-400"
+            href="/auth/signup"
+          >
+            Inscription
+          </Link>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Navbar;
