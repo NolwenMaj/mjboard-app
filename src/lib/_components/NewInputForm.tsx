b9fb22cc4dfcd0ee846a9771adcb7b623678a8a6
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2024-06-14 10:45:25 +0200

    feat(journal) reset form on submit

--- src/lib/_components/NewInputForm.tsx
+++ src/lib/_components/NewInputForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { postJournal } from "../serverAction/journal/postJournal";
 
@@ -7,15 +8,24 @@ type FormValues = {
 };
 
 const NewInputForm = () => {
-  const { register, handleSubmit } = useForm<FormValues>();
+  const { register, handleSubmit, reset, formState } = useForm<FormValues>();
 
-  const onSubmit: SubmitHandler<FormValues> = (newJournal) =>
-    postJournal(newJournal);
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState, reset]);
 
+  const onSubmit: SubmitHandler<FormValues> = async (newJournal) => {
+    try {
+      postJournal(newJournal);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input defaultValue="Ma journée était ..." {...register("content")} />
-
       <input type="submit" />
     </form>
   );
