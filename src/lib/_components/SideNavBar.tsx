422c06432d527278c7e2e4a07b92d7bce972a493
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2024-07-05 15:36:09 +0200

    refacto (journals) : fix naming

--- src/lib/_components/SideNavBar.tsx
+++ src/lib/_components/SideNavBar.tsx
@@ -5,8 +5,8 @@ import { ScrollIcon } from "./ui/customIcons";
 const SideNavBar = () => {
   const tabsUserConnected: NavBarSectionProps[] = [
     {
-      title: "Notes séances",
-      path: "/dashboard-notes",
+      title: "Journaux",
+      path: "/journals",
       icon: <ScrollIcon className="size-4" />,
     },
     {
@@ -17,20 +17,18 @@ const SideNavBar = () => {
   ];
 
   return (
-    <>
-      <nav className="border-r bg-white py-4 md:py-8">
-        <div className="flex-1 flex-col items-start gap-1 overflow-y-auto px-4">
-          {tabsUserConnected.map((tab) => (
-            <NavBarSection
-              title={tab.title}
-              path={tab.path}
-              icon={tab.icon}
-              key={tab.title}
-            />
-          ))}
-        </div>
-      </nav>
-    </>
+    <nav className="border-r bg-white py-4 md:py-8">
+      <div className="flex-1 flex-col items-start gap-1 overflow-y-auto px-4">
+        {tabsUserConnected.map((tab) => (
+          <NavBarSection
+            title={tab.title}
+            path={tab.path}
+            icon={tab.icon}
+            key={tab.title}
+          />
+        ))}
+      </div>
+    </nav>
   );
 };
 export default SideNavBar;
