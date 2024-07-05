2577524231437bf254a9c6025c4700f3ae6658d9
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2024-07-05 15:33:05 +0200

    refacto : no need of client component + disable eslint rule for special classname

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Link from "next/link";
 
 export default function Home() {
@@ -8,6 +7,7 @@ export default function Home() {
       <Link
         href="/sign-in"
         role="button"
+        // eslint-disable-next-line tailwindcss/no-custom-classname
         className="text-grey-900 m-1 flex items-center gap-3 rounded-lg bg-gray-300 p-3  text-center transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
       >
         Authentification
