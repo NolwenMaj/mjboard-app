c9bf21a8c67d0b51c915bf854bb4cbecd0510eee
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2024-07-12 12:39:59 +0200

    feat(Journals): text to show when no journal posted yet

--- src/lib/_components/journals/JournalsScrollview.tsx
+++ src/lib/_components/journals/JournalsScrollview.tsx
@@ -4,7 +4,15 @@ import JournalCard from "./JournalCard";
 export const JournalsScrollView = async () => {
   const journals = await getAllJournals();
 
-  return journals
-    ? journals.map((journal) => <JournalCard key={journal.id} {...journal} />)
-    : null;
+  return !journals || journals.length === 0 ? (
+    <div className="flex size-full flex-col items-center justify-center">
+      <p className="text-xl font-bold">Pas encore de journal à afficher</p>
+    </div>
+  ) : (
+    <div className="flex-col-reverse">
+      {journals?.map((journal) => (
+        <JournalCard key={journal.id} {...journal} />
+      ))}
+    </div>
+  );
 };
